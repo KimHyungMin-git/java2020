3c0ccdb1b8d614ba6466bb188e7ac46ecf5ba28d
KimHyungMin-git <21900221@handong.edu>
2020-06-07 22:17:45 +0900

    CH08_2

---
+++ CH08_Book_main.java
@@ -0,0 +1,10 @@
+public class CH08_Book_main {
+	public static void main(String[] args) {
+		CH08_Book book= new CH08_Book("Simple Book");
+		CH08_Science science= new CH08_Science("Hello Physics!", "ScienceWorld"); // Name, Publisher
+		CH08_History history = new CH08_History("What Is history?", "E.H.Carr");// Name, Author
+		book.print();
+		science.print();
+		history.print();}
+	}
+
