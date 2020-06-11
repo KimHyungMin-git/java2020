4465da06c1388baee52f5c191f311180a5e099a0
KimHyungMin-git <21900221@handong.edu>
2020-06-11 18:57:40 +0900

    CH12_3

---
+++ CH12_NumIntegers_main.java
@@ -0,0 +1,11 @@
+public class CH12_NumIntegers_main {
+	public static void main(String[] args) {
+		String fileName = "CH12_numbers.txt";
+		CH12_NumIntegers NI = new CH12_NumIntegers();
+		System.out.println("Reading integers from the file.");
+		NI.readdata(fileName);
+		System.out.println("Reading Done!");
+		System.out.print("\nThe file contains ");
+		System.out.println(NI.getNumIntegers() + " different integers.\n");
+		NI.printIntegers();}
+}
