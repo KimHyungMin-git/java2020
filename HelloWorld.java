d614db27ec750c7a1d5a27d006937237042983c0
KimHyungMin-git <21900221@handong.edu>
2020-06-15 17:01:50 +0900

    CH01_4

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class HelloWorld {
+	public static void main(String [] args) {
+		int i,c=1;
+		for(i=0; i<10; i++);
+		c=c+i;
+		System.out.println(c);
+	}
+}
+
---
+++ KimHyungMin.java
@@ -0,0 +1,7 @@
+public class KimHyungMin {
+	public static void main(String[] args) {
+		System.out.println("Name : KimHyungMin");
+		System.out.println("University : Handong Global University");
+		System.out.println("Student Number 21900221");
+	}
+}
