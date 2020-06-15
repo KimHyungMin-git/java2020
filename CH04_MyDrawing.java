a00880d268e227468ace1cc5d41b599f77bc05bd
KimHyungMin-git <21900221@handong.edu>
2020-06-15 17:03:26 +0900

    CH04_1

---
+++ CH04_MyDrawing.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class CH04_MyDrawing {
+	public static void main(String arg[]) {
+		String test = "abcdefg";
+				System.out.println(test.length());
+				System.out.println(test.charAt(1); 
+	}
+}
+
